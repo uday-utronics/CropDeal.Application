48e4f3fe0aca37ce6c173a6c57831a29022ac3e9
ubanda <ubanda@LIN66013053.corp.capgemini.com>
2021-10-18 22:16:20 +0530

    swagger implemented

--- FarmerMicroservice/src/main/java/com/farmer/FarmerMicroserviceApplication.java
+++ FarmerMicroservice/src/main/java/com/farmer/FarmerMicroserviceApplication.java
@@ -6,12 +6,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableDiscoveryClient
+@EnableSwagger2
 public class FarmerMicroserviceApplication {
 
 	@Bean
